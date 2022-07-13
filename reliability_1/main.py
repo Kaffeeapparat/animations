a0f747de76a1e4aa3d8dcfc68cc60bcbfbc47957
Kaffeeapparat <28804780+Kaffeeapparat@users.noreply.github.com>
2022-07-13 06:45:45 +0200

    Added basic video outline
    The #! comments show the different video sections/subtopics.

---
+++ reliability_1/main.py
@@ -0,0 +1,48 @@
+from manim import *
+from numpy import *
+
+
+config.background_color= DARK_GREY
+config.quality='medium_quality'
+
+
+
+class Reliability(Scene):
+    def construct(self):
+
+        #!What is relaiability exactly
+
+        #!On different elctronic devices like Harddrives are printed some numbers
+
+
+        #!Will it really run 4 million hours, and why does it seem that modern car break down more regularly
+
+        #!Of course  it is only a statistical figure
+
+        #!Bathtub curve
+
+        #!"Electronic devices fail with following distribution", early, random and wearout failures
+
+        #!Distingushing the important part of the Bathtub and the nonimportant part of the bathtub
+
+        #!Brilliantly modeling the exp(-tλ) curve in relationship to bathtub curve
+
+        #!Calculating lambda
+
+        #!Modeling some examples, headlights for example
+
+        #!The task with the drinking water system is funny
+
+        #!Dive into availability with repair time figures
+
+        #!Decreaseing  Reliability
+
+        #!Ambient system influences, look in script about silicon lifetime vs tempature
+
+        #!Increase reliability
+
+        #!Parallel systems
+
+
+
+
