3dc4e70e852c15b48628b276b73ad66ab0f70d62
Kaffeeapparat <28804780+Kaffeeapparat@users.noreply.github.com>
2022-07-14 19:50:50 +0200

    Adding basic draft for title slide

--- reliability_1/main.py
+++ reliability_1/main.py
@@ -27,8 +27,30 @@ class Reliability(Scene):
 
         #!What is relaiability exactly
 
-        #!On different elctronic devices like Harddrives are printed some numbers
+        s1_hdd=SVGMobject("graphics/harddrive.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).rotate(120)
 
+        t1=Tex(r"What is reliability?",font_size=60)
+
+        t2_hdd=Tex(r"MTBF\\4 million hours",font_size=23).rotate(120)
+        t4=Tex(r"MTTF").rotate(-23)
+        t5=Tex(r"MTTR").rotate(56)
+        t6=Tex(r"Seems to never break")
+        t7=Tex(r"Just needs new Caps")
+
+        g1_title= VGroup(t1,t4).arrange(DOWN, buff=1)
+        g1_title.move_to(UP*1.5)
+
+        g2_hdd=VGroup(t2_hdd,s1_hdd).arrange(DOWN).move_to(DOWN*2+RIGHT*5)
+
+        g1_fadeout=(FadeOut(g1_title,shift=UP))
+
+
+        self.add(g1_title,g2_hdd)
+        self.wait(1)
+        self.play(g1_fadeout)
+        self.wait(1)
+
+        #!On different elctronic devices like Harddrives are printed some numbers
 
         #!Will it really run 4 million hours, and why does it seem that modern car break down more regularly
 
