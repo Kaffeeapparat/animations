a256012c8f4a02401660111c1a70876c96c3cd1b
Kaffeeapparat <28804780+Kaffeeapparat@users.noreply.github.com>
2022-07-16 17:00:57 +0200

    Changing the variables of slide 5 to slide 3
    Variable change from var number 05 to var number 03

--- reliability_1/main.py
+++ reliability_1/main.py
@@ -69,7 +69,7 @@ class Reliability(Scene):
         #!Bathtub curve
 
         #Basic Axes, length can't be changed
-        #o05x1=Axes(x_range=[0,3,1],
+        #o03x1=Axes(x_range=[0,3,1],
                         #y_range=[0, 1, 1],
                         #x_length=5,
                         #y_length=3,
@@ -77,11 +77,11 @@ class Reliability(Scene):
         #)
 
         #Axes with ValueTracker, to change parameters and enable animations
-        v05x1=ValueTracker(2)
-        o05x1=always_redraw(lambda:Axes
+        v03x1=ValueTracker(2)
+        o03x1=always_redraw(lambda:Axes
                                 (x_range=[0,3,1],
                                 y_range=[0, 1, 1],
-                                x_length=v05x1.get_value(),
+                                x_length=v03x1.get_value(),
                                 y_length=3,
                                 tips=True,
                                 )
@@ -89,21 +89,21 @@ class Reliability(Scene):
 
 
         #Functions to indicate the early stage, random stage, and wearout failures
-        o05x2=always_redraw(lambda: o05x1.plot
+        o03x2=always_redraw(lambda: o03x1.plot
                                 (lambda x:np.exp(-x)-0.3,
                                     x_range=[0,1],
                                     use_smoothing=False
                                 )
                             )
 
-        o05x3=always_redraw(lambda: o05x1.plot
+        o03x3=always_redraw(lambda: o03x1.plot
                                 (lambda x:np.exp(-1)-0.3,
                                 x_range=[1,2],
                                 use_smoothing=False
                                 )
                             )
 
-        o05x4=always_redraw(lambda: o05x1.plot
+        o03x4=always_redraw(lambda: o03x1.plot
                                 (lambda x:
                                 np.exp(x-3)-0.3,
                                 x_range=[2,3],
@@ -111,37 +111,37 @@ class Reliability(Scene):
                                 )
                             )
 
-        o05x5=Brace(o05x3)
-        o05x6=Brace(o05x2)
-        o05x7=Brace(o05x4)
-        t05x1=Tex(r"early\\failures")
-        t05x2=Tex(r"random\\failures")
-        t05x3=Tex(r"wearout\\failures")
+        o03x5=Brace(o03x3)
+        o03x6=Brace(o03x2)
+        o03x7=Brace(o03x4)
+        t03x1=Tex(r"early\\failures")
+        t03x2=Tex(r"random\\failures")
+        t03x3=Tex(r"wearout\\failures")
 
-        g05x1_ef=VGroup(o05x5,t05x1).arrange(DOWN,buff=0.5).next_to(o05x2,DOWN)
-        g05x2_rf=VGroup(o05x6,t05x2).arrange(DOWN,buff=0.5).next_to(o05x3,DOWN)
-        g05x3_wf=VGroup(o05x7,t05x3).arrange(DOWN,buff=0.5).next_to(o05x4,DOWN*1.09)
+        g03x1_ef=VGroup(o03x5,t03x1).arrange(DOWN,buff=0.5).next_to(o03x2,DOWN)
+        g03x2_rf=VGroup(o03x6,t03x2).arrange(DOWN,buff=0.5).next_to(o03x3,DOWN)
+        g03x3_wf=VGroup(o03x7,t03x3).arrange(DOWN,buff=0.5).next_to(o03x4,DOWN*1.09)
 
         self.wait(1)
         self.clear
-        v05x1.set_value(10)
-        self.play(FadeIn(o05x1))
+        v03x1.set_value(10)
+        self.play(FadeIn(o03x1))
         self.wait(1)
-        self.play(v05x1.animate.set_value(12))
+        self.play(v03x1.animate.set_value(12))
 
-        o05x2.update()
-        o05x3.update()
-        o05x4.update()
+        o03x2.update()
+        o03x3.update()
+        o03x4.update()
 
-        self.play(o05x2.animate.set_color(PINK))
-        o05x2.update()
+        self.play(o03x2.animate.set_color(PINK))
+        o03x2.update()
         self.wait(1)
-        self.play(o05x4.animate.set_color(PINK))
+        self.play(o03x4.animate.set_color(PINK))
         self.wait(1)
-        self.play(o05x3.animate.set_color(YELLOW))
-        self.play(FadeIn(g05x1_ef))
-        self.play(FadeIn(g05x2_rf))
-        self.play(FadeIn(g05x3_wf))
+        self.play(o03x3.animate.set_color(YELLOW))
+        self.play(FadeIn(g03x1_ef))
+        self.play(FadeIn(g03x2_rf))
+        self.play(FadeIn(g03x3_wf))
 
         self.wait(1)
         self.clear()
@@ -169,13 +169,16 @@ class Reliability(Scene):
 
         #!Increase reliability with parallel systems
 
+
         #Big Axes that shows the effect on adding multiple devices in parallel on MTBF
+
+
 
         o08x1=Axes(tips=False,
                  x_range=[0,1,1],
                  y_range=[0,1,1],
                  x_length=12,
-                 y_length=6,
+                 y_length=5,
                  )
 
 
@@ -191,7 +194,7 @@ class Reliability(Scene):
                                 )
 
         self.clear()
-        self.add(o08x1)
+        self.play(o08x1.animate)
         for i in range(len(h08x1)):
             #self.play(FadeIn(h08x1[i]))
             self.add(h08x1[i])
