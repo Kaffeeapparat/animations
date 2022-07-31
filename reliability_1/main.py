8be9c5e5cf481f65fb941431341432a815146ce4
Kaffeeapparat <28804780+Kaffeeapparat@users.noreply.github.com>
2022-07-31 08:58:35 +0200

    Polishing everything
    A lot of polishing

--- reliability_1/main.py
+++ reliability_1/main.py
@@ -45,8 +45,6 @@ class Intro(Scene):
 
         s01x1_hdd=SVGMobject("graphics/harddrive.svg",fill_opacity=1.0).to_corner(LEFT)
 
-
-
         t01x1=Tex(r"Uncovering th mystery\\of reliability",font_size=80).move_to(ORIGIN)
         t01x2_hdd=Tex(r"HDD MTTF\\1 million hours!!!",font_size=30).rotate(PI*-0.1)
         g01x2_hdd=VGroup(s01x1_hdd,t01x2_hdd).arrange(UP, buff=0.1).move_to(DOWN*2+RIGHT*5)
@@ -78,31 +76,44 @@ class Definition(Scene):
         t02x2=Tex(r"State of the Device:")
         t02x3=Tex(r"defect",r"functioning",font_size=50).arrange(DOWN)
         g02x3_bestate=VGroup(t02x2,t02x3).arrange(DOWN)
+        t02x11=Tex(r"Definition",font_size=80)
+        t02x12=Tex(r"Device",font_size=60)
 
         #tex.animate.set_color(RED),
         #be stands for Bauelement, the basic bulding block of devices
         s02x1_be=SVGMobject("graphics/device.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
-
         s02x2_be=SVGMobject("graphics/device_connected.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
-
         s02x3_d1=SVGMobject("graphics/diode.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
-
         s02x4_d2=SVGMobject("graphics/diode2.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
+        s02x5_cap=SVGMobject("graphics/cap.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
+        s02x8_complexbe=SVGMobject("graphics/complex_device.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).move_to(LEFT*3).scale(0.5)
 
-        s02x2_cap=SVGMobject("graphics/cap.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
+        self.play(FadeIn(t02x11))
+        self.wait(1)
+        self.play(FadeOut(t02x11))
+        self.clear
+        self.wait(1)
 
         g02x1=VGroup()
         for i in range(1,10,1):
             g02x1.add(SVGMobject("graphics/device_connected.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.3))
         g02x1.arrange(DOWN,buff=0)
 
-        g02x2=VGroup(s02x3_d1,s02x4_d2,s02x2_cap)
+        g02x2=VGroup(s02x3_d1,s02x4_d2,s02x5_cap,s02x8_complexbe,s02x1_be)
+        s02x1_be=SVGMobject("graphics/device.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
+
 
         self.add(s02x1_be)
         o01x1=s02x1_be
-        for i in range(0,3,1):
+        t02x12.move_to(s02x1_be.get_center()+UP)
+        self.play(FadeIn(t02x12))
+
+        for i in range(0,5,1):
             self.play(ReplacementTransform(o01x1,g02x2[i]))
             o01x1=g02x2[i]
+
+
+
         self.wait(1)
         g02x3_bestate.move_to(ORIGIN+RIGHT*3)
         self.play(Create(g02x3_bestate))
@@ -159,11 +170,12 @@ class Definition(Scene):
         )
         o02x05_origindot=Dot()
         self.play(ReplacementTransform(t02x10_ratec,t02x5_ratec),FadeOut(t02x5_ratec[3]))
+        self.wait(1)
         g02x8=VGroup(t02x5_ratec[0],t02x5_ratec[1],t02x5_ratec[2])
         g02x8.generate_target()
         g02x8.target.shift(RIGHT*3.5)
         self.play(MoveToTarget(g02x8))
-
+        self.wait(1)
         o02x01_n=DecimalNumber(
             100,
             show_ellipsis=False,
@@ -201,12 +213,15 @@ class Definition(Scene):
 
         #self.add(index_labels(t02x5_ratec[0]))
         self.play(ShrinkToCenter(t02x5_ratec[0]),FadeIn(o02x04_lambda.move_to(t02x5_ratec[0].get_center())))
-
+        self.wait(1)
         self.play(ShrinkToCenter(t02x5_ratec[2][0:2]),FadeIn(o02x02_dn.move_to(t02x5_ratec[2][0:2].get_center())))
 
+        self.wait(1)
         o02x01_n.move_to(t02x5_ratec[2][3:4].get_center()+UP*0.05)
         self.play(ShrinkToCenter(t02x5_ratec[2][3:4]),FadeIn(o02x01_n.move_to(t02x5_ratec[2][3:4].get_center()+UP*0.11)))
 
+
+        self.wait(1)
         self.play(ShrinkToCenter(t02x5_ratec[2][5:7]),FadeIn(o02x03_dt.move_to(t02x5_ratec[2][5:7].get_center())))
 
         #self.add(VGroup(o02x01_n,o02x02_dn,o02x03_dt).arrange(DOWN).to_corner(DOWN,LEFT))
@@ -238,7 +253,7 @@ class Definition(Scene):
 
         #!Of course  it is only a statistical figure
 
-        t02x6=MathTex(r"\text{MTTF,MTTR}=\frac{1}{\lambda}").move_to(UP*2)
+        t02x6=MathTex(r"\text{MTBF,MTTF}=\frac{1}{\lambda}").move_to(UP*2)
         t02x8=Tex(r"MTBF:",r"Mean Time between failure",r"$\implies$repairable")
         t02x7=Tex(r"MTTF:",r"Mean Time to failure",r"$\implies$unrepairable").move_to(DOWN*2)
         s02x5_wrench=SVGMobject("graphics/wrench.svg",stroke_color=GREEN,fill_color=GREEN,fill_opacity=1.0).scale(0.5)
@@ -257,14 +272,19 @@ class Definition(Scene):
         self.wait(1)
         self.play(Create(s02x6_rec.next_to(g02x7,RIGHT)))
         self.wait(1)
+        self.clear()
 
 class Bathtub(Scene):
     def construct(self):
 
         #!Bathtub curve
         t03x1=Tex(r"Typical failure distribution of electronic systems")
+        t03x8=Tex(r"Failure distributions",font_size=50)
 
-
+        self.play(FadeIn(t03x8))
+        self.wait(1)
+        self.play(FadeOut(t03x8))
+        self.wait(1)
 
         #Basic Axes, length can't be changed
         #o03x1=Axes(x_range=[0,3,1],
@@ -351,10 +371,10 @@ class Bathtub(Scene):
         self.play(AnimationGroup(*[Create(s) for s in g03x5_bathtub],lag_ratio=1))
 
         #Animate the brace fade in after the color shift
-        self.play(o03x2.animate.set_color(BLUE_A))
+        self.play(o03x2.animate.set_color(BLUE_D))
         self.play(FadeIn(g03x1_ef))
         self.wait(1)
-        self.play(o03x4.animate.set_color(BLUE_A))
+        self.play(o03x4.animate.set_color(BLUE_D))
         self.play(FadeIn(g03x3_wf))
         self.wait(1)
         self.play(o03x3.animate.set_color(YELLOW_C))
@@ -381,15 +401,15 @@ class RandomFail(Scene):
         t04x1_Rt=MathTex(r"R\left(t\right)=",r"e",r"^{-t",r"\lambda}")
         t04x2_Ft=MathTex(r"F\left(t\right)=1-R\left(t\right)")
         g04x1_f=VGroup(t04x1_Rt,t04x2_Ft).arrange(DOWN).move_to(UP*2+RIGHT*2)
-        t04x3_poi=MathTex(r"\text{MTBF}",font_size=40)
+        t04x3_poi=MathTex(r"\text{MTBF,MTTF}",font_size=40)
         t04x4_ylb=Tex(r"R(t)",r"F(t)")
         t04x4_ylb.arrange(DOWN)
         t04x4_ylb[0].set_color(YELLOW_C)
-        t04x4_ylb[1].set_color(BLUE)
+        t04x4_ylb[1].set_color(BLUE_D)
 
         t04x5_val=MathTex(r"0.63",r"0.37")
         t04x5_val.arrange(DOWN)
-        t04x5_val[0].set_color(BLUE)
+        t04x5_val[0].set_color(BLUE_D)
         t04x5_val[1].set_color(YELLOW_C)
 
         o04x1=Axes(x_range=[0,3,1],
@@ -418,9 +438,10 @@ class RandomFail(Scene):
                             )
 
         t04x4_ylb.next_to(o04x4_ylb,RIGHT)
-        g04x1_initstart=VGroup(o04x1,o04x5_rt,o04x4_xlb,t04x4_ylb[0])
+        g04x1_initstart=VGroup(o04x1,o04x4_xlb,t04x4_ylb[0])
 
         self.play(Create(g04x1_initstart))
+        self.play(Create(o04x5_rt,run_time=2))
         self.wait(1)
         self.wait(1)
         #self.play()
@@ -441,7 +462,7 @@ class RandomFail(Scene):
         self.play(FadeIn(t04x3_poi.next_to(o04x1.c2p(1,0),DOWN)))
         self.play(Create(g04x1_f[0]))
         self.wait(1)
-        self.play(Create(o04x5_ft),FadeIn(t04x4_ylb[1]))
+        self.play(Create(o04x5_ft,run_time=2),FadeIn(t04x4_ylb[1]))
         self.play(Create(o04x8))
         self.play(Create(g04x1_f[1]))
         self.wait(1)
@@ -461,10 +482,18 @@ class Headlightexample(Scene):
 
         t06x1=Tex(r"MTTR")
 
+        self.clear()
+
 class Serialparallel(Scene):
     def construct(self):
         #!In Real world systems are build out of many parts. They influence the reliability, too.
 
+        t07x8=Tex(r"Evaluating serial and parallel systems",font_size=50)
+
+        self.play(FadeIn(t07x8))
+        self.wait(1)
+        self.play(FadeOut(t07x8))
+        self.wait(1)
 
         t07x1=Tex(r"Parallel systems",r"\\ rendundancy").move_to(UP*3+LEFT*4)
 
@@ -474,7 +503,7 @@ class Serialparallel(Scene):
 
         s07x2_bev=SVGMobject("graphics/device_connected.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).rotate(PI*0.5).scale(0.3)
 
-        t07x3_cont=MathTex(r"      ...      ")
+        t07x3_cont=MathTex(r".     ...     .")
         t07x4_cont=MathTex(r"      ...      ")
 
         g07x1_ser=VGroup()
@@ -485,7 +514,7 @@ class Serialparallel(Scene):
             g07x1_ser.add(SVGMobject("graphics/device.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.2))
 
         g07x1_ser.add(t07x3_cont,s07x1_beh)
-        g07x1_ser.arrange(RIGHT,buff=0.01).move_to(UP*1.5+RIGHT*4)
+        g07x1_ser.arrange(RIGHT,buff=-0.04).move_to(UP*1.5+RIGHT*4)
 
         #!Increase reliability with parallel systems
 
@@ -496,7 +525,8 @@ class Serialparallel(Scene):
 
         g07x2_par.add(t07x4_cont,s07x2_bev).arrange(RIGHT,buff=0.006).move_to(UP*1.5+LEFT*4)
 
-        self.add(t07x1,t07x2,g07x1_ser,g07x2_par)
+        self.play(FadeIn(t07x1[0]))
+        self.play(Create(g07x2_par))
         self.wait(1)
 
         t07x5_sRt=MathTex(r"R_{serial}\left( t \right)=\prod_{i=1}^{i}R_{i}\left( t \right)=R_{1}\left( t \right)\cdot R_{2}\left( t \right)\cdot ...",font_size=35)
@@ -505,7 +535,7 @@ class Serialparallel(Scene):
         t07x5_sRt.move_to(RIGHT*3.5)
         t07x6_pRt.move_to(LEFT*3.5)
 
-        self.play(Create(t07x5_sRt),Create(t07x6_pRt))
+        self.play(Create(t07x6_pRt))
 
 
         o07x1_axp=Axes(tips=False,
@@ -524,11 +554,11 @@ class Serialparallel(Scene):
             g07x3_axp.add(o07x1_axp.plot
                       (lambda x: 1-(1-exp(-5*x))**i,
                             x_range=[0,1],
-                            color=Color(hue=i/10,saturation=1,luminance=0.5)
+                            color=Color("red",hue=(i-0.9999)/(13),)
                         )
                     )
 
-        self.add(o07x1_axp)
+        self.play(FadeIn(o07x1_axp))
         self.play(AnimationGroup(*[FadeIn(s) for s in g07x3_axp],lag_ratio=1))
         self.wait(5)
 
@@ -550,12 +580,17 @@ class Serialparallel(Scene):
             g07x4_axs.add(o07x2_axs.plot
                       (lambda x: ((exp(-5*x))**i),
                             x_range=[0,1,0.001],
-                            color=Color(hue=i/10,saturation=1,luminance=0.5),
+                            color=Color("red",hue=(i-0.9999)/(13),),
                             use_smoothing=False,
                         )
                     )
-
-        self.add(o07x2_axs)
+        self.play(FadeIn(t07x2))
+        self.wait(1)
+        self.play(Create(g07x1_ser))
+        self.wait(1)
+        self.play(Create(t07x5_sRt))
+        self.wait(1)
+        self.play(FadeIn(o07x2_axs))
         self.play(AnimationGroup(*[FadeIn(s) for s in g07x4_axs],lag_ratio=1))
         self.wait(5)
 
@@ -605,7 +640,7 @@ class Economics(Scene):
         for i in range(1,10,1):
             g10x1.add(o10x1.plot(o10x4_fb(i),
                         x_range=[0,1,0.001],
-                        color=Color(hue=i/10,saturation=1,luminance=0.5),
+                        color=Color("red",hue=(i-0.9999)/(13)),
                         use_smoothing=False
                         )
                     )
@@ -618,51 +653,64 @@ class Economics(Scene):
         self.play(AnimationGroup(*[FadeIn(s) for s in g10x1],lag_ratio=1))
         self.wait(2)
 
-        t10x2=MathTex(r"r=0",r"r=1",r"r=2",r"r=3",r"r=4",r"r=5",r"r=6",r"r=7",r"r=8",font_size=20)
+        t10x2=MathTex(r"r=0",r"r=1",r"r=2",r"r=3",r"r=4",r"r=5",r"r=6",r"r=7",r"r=8",r"r=9",font_size=20)
         g10x3=VGroup()
-        h10x1=[0.05,0.075,0.15,0.25,0.35,0.45,0.55,0.65,0.75]
-        for i in range(0,9,1):
-            g10x3.add(Rectangle(height=0.5,width=1,fill_color=LIGHT_BROWN,color=Color(hue=(i+1)/10,saturation=1,luminance=0.5),fill_opacity=1).move_to(o10x1.c2p(h10x1[i],
-                                (o10x4_fb(i+1)(h10x1[i]))
+        h10x1=[0.00,0.05,0.08 ,0.15,0.25,0.35,0.45,0.55,0.65,0.75,0.9]
+        for i in range(1,10,1):
+            g10x3.add(Rectangle(height=0.5,width=1,fill_color=LIGHT_BROWN,color=Color("red",hue=(i-0.9999)/(13),),fill_opacity=1).move_to(o10x1.c2p(h10x1[i],
+                                (o10x4_fb(i)(h10x1[i]))
                                 ))
                         )
             g10x3.add(t10x2[i].move_to(o10x1.c2p(h10x1[i],
-                                (o10x4_fb(i+1)(h10x1[i]))
+                                (o10x4_fb(i)(h10x1[i]))
                                 )))
 
-        self.add(g10x3)
+        self.play(AnimationGroup(*[Create(s) for s in g10x3],lag_ratio=1,run_time=3))
         self.wait(1)
 
 
 
-        t10x3=MathTex(r"MTBF/\\MTTF ",font_size=30)
+        t10x3=MathTex(r"MTBF,MTTF ",font_size=30)
         o10x6=Line(o10x1.c2p(0.2,0),o10x1.c2p(0.2,1)).set_color(YELLOW_B)
         g10x4=VGroup(o10x6,t10x3)
         t10x3.next_to(o10x6,DOWN)
         self.add(g10x4)
         self.wait(1)
 
-        self.play(FadeOut(g10x3))
 
         self.wait(1)
         h10x2=o10x1.c2p(0.1,0.5)
         h10x3=o10x1.c2p(0.2,1)
 
         o10x7=Rectangle(height=1*o10x1.get_y_unit_size(),width=0.2*o10x1.get_x_unit_size(),fill_opacity=0.5,fill_color=GREEN,stroke_opacity=1,stroke_width=0).move_to(h10x2)
-        self.add(o10x7)
         self.wait(1)
+
+        h10x2=[2,4,5,6,7]
+
+        for i in h10x2:
+            self.play(FadeOut(g10x3[i*2],g10x3[(i*2+1)]))
+            self.play(FadeOut(g10x1[i]))
+        self.wait(2)
+
+        self.play(FadeIn(o10x7))
+        self.wait(2)
         self.clear()
-
-
-        #!Ambient system influences, look in script about silicon lifetime vs tempature
 
 class Enviroment(Scene):
     def construct(self):
-        t11x1=(r"Considering the enviroment")
+        #!Ambient system influences, look in script about silicon lifetime vs tempature
+
+        t11x1=Tex(r"Enviromental Factors",font_size=50)
+        self.play(FadeIn(t11x1))
+        self.wait(1)
+        self.play(FadeOut(t11x1))
+        self.wait(1)
+
+        t11x7=Tex(r"Enviromental Factors",font_size=50)
+        t11x7.move_to(UP*3)
 
         s11x1_be=SVGMobject("graphics/device.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).to_corner(LEFT).scale(0.5)
-        s11x1_be.move_to(ORIGIN)
-
+        s11x1_be.move_to(DOWN)
         s11x2_heat=SVGMobject("graphics/heat.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).scale(0.5).next_to(s11x1_be,UP*3+RIGHT*3)
         s11x3_voltage=SVGMobject("graphics/voltage.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0).scale(0.5).next_to(s11x1_be,UP*3+LEFT*3)
 
@@ -673,19 +721,31 @@ class Enviroment(Scene):
         t11x2_uv=MathTex(r"UV")
         t11x3_temp=MathTex(r"Temp")
         t11x4_U=MathTex(r"U(t)")
-        t11x5_oth=MathTex(r"...").next_to(s11x1_be,UP*5)
+        t11x5_oth=MathTex(r"...").next_to(s11x1_be,UP*9)
 
-        a11x3_punkt=Arrow().put_start_and_end_on(t11x5_oth.get_center(),s11x1_be.get_center()).scale(0.3)
+        a11x3_oth=Arrow().put_start_and_end_on(t11x5_oth.get_center(),s11x1_be.get_center()).scale(0.3)
 
         t11x6_form=MathTex(r"\lambda=\lambda_{ref}\cdot\pi_{temp}\cdot\pi_{humidity}\cdot\pi_{...}")
         t11x6_form.move_to(DOWN*3)
 
+        g11x1=(s11x2_heat,a11x1_heat,s11x3_voltage,a11x2_temp,t11x5_oth,a11x3_oth)
+
         self.add(s11x1_be)
         self.wait(0.5)
-        self.add(s11x2_heat,s11x3_voltage,t11x5_oth)
-        self.wait(0.5)
-        self.add(a11x1_heat,a11x2_temp,a11x3_punkt)
-        self.add(t11x6_form)
+        s=range(0,1)
+        self.play(Create(s11x2_heat),Create(a11x1_heat))
+        self.wait(1)
+        self.play(s11x2_heat.animate.set(color=RED))
+        self.wait(2)
+        self.play(Create(s11x3_voltage),Create(a11x2_temp))
+        self.wait(1)
+        self.play(s11x3_voltage.animate.set(color=RED))
+        self.wait(2)
+        self.play(Create(t11x5_oth),Create(a11x3_oth))
+        self.wait(1)
+        self.play(t11x5_oth.animate.set(color=RED))
+        self.wait(2)
+        self.play(Create(t11x6_form))
         self.wait(2)
         self.clear()
 
@@ -693,7 +753,15 @@ class Availability(Scene):
     def construct(self):
         #! Explaining availability
 
-        t12x1=Tex(r"How high is the availability\\of the system lamp?").move_to(UP*3)
+        t12x10=Tex(r"Availability",font_size=50)
+
+        self.play(FadeIn(t12x10))
+        self.wait(1)
+        self.play(FadeOut(t12x10))
+        self.wait(1)
+
+
+        t12x1=Tex(r"How high is the availability of a system",font_size=50).move_to(UP*3)
         t12x2=MathTex(r"\text{A}=\frac{\text{MTBR}}{\text{MTBR}\cdot\text{MTTR}}")
         t12x4=MathTex(r"MTTR_{changelightbulb}=2h",r"MTTR_{system}=HIGH",r"MTTR_{system}=LOW")
         t12x3=MathTex(r"MTBF_{lightbulb}=1000h^{-1}",r"MTBF_{repair}=LOW")
@@ -701,8 +769,8 @@ class Availability(Scene):
         t12x6=MathTex(r"0.998",r"=\frac{1000}{1000\cdot{2}")
         t12x7=MathTex(r"0.002",r"=1- \frac{1000}{1000\cdot{2}")
 
-        t12x8=MathTex(r"\text{LOW}=\frac{\text{LOW}}{\text{LOW}\cdot\text{HIGH}}")
-        t12x9=MathTex(r"\text{HIGH}=\frac{\text{LOW}}{\text{LOW}\cdot\text{LOW}}")
+        t12x8=MathTex(r"A=",r"\text{LOW}=\frac{\text{LOW}}{\text{LOW}\cdot\text{HIGH}}")
+        t12x9=MathTex(r"A=",r"\text{HIGH}=\frac{\text{LOW}}{\text{LOW}\cdot\text{LOW}}")
 
         s12x1_lampwork=SVGMobject("graphics/lamp_on.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0)
         s12x2_lampnowork=SVGMobject("graphics/lamp_off.svg",stroke_color=WHITE,fill_color=WHITE,fill_opacity=1.0)
@@ -714,6 +782,9 @@ class Availability(Scene):
         s12x1_lampwork.move_to(LEFT*2)
         s12x2_lampnowork.move_to(s12x1_lampwork.get_center()+LEFT*0.2).set
         g12x2=VGroup(s12x1_lampwork,s12x2_lampnowork)
+
+        self.play(Create(t12x1))
+        self.wait(1)
 
         self.play(Create(s12x2_lampnowork))
         self.wait(1)
@@ -729,8 +800,8 @@ class Availability(Scene):
         self.wait(1)
 
         s12x2_lampnowork.set(color=WHITE)
+        s12x1_lampwork.set(color=WHITE)
         self.play(g12x2.animate.to_corner(LEFT))
-        self.play(Create(t12x1))
         self.wait(1)
 
 
@@ -761,21 +832,22 @@ class Availability(Scene):
         self.play(s12x1_lampwork.animate.set(stroke_color=GREEN),s12x2_lampnowork.animate.set(stroke_color=GREEN),t12x6[0].animate.set(color=GREEN))
         self.wait(1)
         self.play(ReplacementTransform(t12x6,t12x7))
-        self.wait(0,5)
+        self.wait(1)
         self.play(t12x7[0].animate.set(color=RED),s12x2_lampnowork.animate.set(stroke_color=RED),FadeOut(s12x1_lampwork))
         self.wait(2)
 
         t12x2=MathTex(r"\text{A}=\frac{\text{MTBR}}{\text{MTBR}\cdot\text{MTTR}}").move_to(g12x1_f[0].get_center())
         self.play(ReplacementTransform(g12x2,s12x5_complexdevice.move_to(g12x2.get_center())),ReplacementTransform(t12x7,t12x2),FadeOut(t12x4[0]),FadeOut(t12x3[0]))
 
-        self.play(FadeIn(t12x4[1].move_to(t12x4[0].get_center())))
         self.play(FadeIn(t12x3[1].move_to(t12x3[0].get_center())))
+        self.play(FadeIn(t12x4[1].move_to(t12x4[0].get_center())))
+
         self.play(ReplacementTransform(t12x2,t12x8.move_to(t12x2.get_center())))
         self.wait(1)
 
         self.play(s12x5_complexdevice.animate.set(stroke_color=RED),t12x4[1][11:15].animate.set(color=RED))
-        self.add(index_labels(t12x4[1]))
-        self.wait(1)
+        #self.add(index_labels(t12x4[1]))
+        self.wait(3)
 
 
         #self.play(ReplacementTransform(t12x9))
